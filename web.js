a17e260f4e67cf970ab3620eb19f7225172acc97
Andreas Agrafiotis <and_agraf@hotmail.com>
2013-07-11 10:17:40 +0000

    Commit web.js v3

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+
 var page = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
   response.send(page.toString('utf8'));
-}
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
