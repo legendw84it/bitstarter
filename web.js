458a47eaa7ae035fe010809bfb5b27e12ca42a80
Andreas Agrafiotis <and_agraf@hotmail.com>
2013-07-11 09:59:11 +0000

    Web.js v2

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var page = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(page.toString('utf8'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
