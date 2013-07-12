02c550b9cdf7326d039e6a700471678b60046df9
Andreas Agrafiotis <and_agraf@hotmail.com>
2013-07-12 14:57:48 +0000

    Add code to pass url to grader

--- grader.js
+++ grader.js
@@ -66,10 +66,11 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <html_file>', 'Path to webpage', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to webpage', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     if(program.url){
-	var url = rest.get(url).on('complete', checkHtmlFile(program.url, program.checks));
+	var url = program.url.toString();
+	rest.get(url).on('complete', checkHtmlFile(program.url, program.checks));
         var outJson = JSON.stringify(checkJson, null, 4);
     }
     else{
